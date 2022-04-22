df866a2d8ac0e9f43d34f1eec162152bcfcc93c1
TaiyangAh <wulian404@gmail.com>
2022-04-22 21:42:31 +0800

    生成接口实例并使用

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ internal is the same as private, except that it's also accessible to contracts t
 
 external is similar to public, except that these functions can ONLY be called outside the contract — they can't be called by other functions inside that contract.
 
-2.For our contract to talk to another contract on the blockchain that we don't own, first we need to define an **interface**. it's syntax like a contract skeleton. This is how the compiler knows it's an interface
+2.For our contract to talk to another contract on the blockchain that we don't own, first we need to define an **interface**. its syntax like a contract skeleton. This is how the compiler knows it's an interface
--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -24,6 +24,10 @@ abstract contract KittyInterface {
 }
 
 contract ZombieFeeding is ZombieFactory {
+    //生成接口Interface实例
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    KittyInterface kittyContract = KittyInterface(ckAddress);
+
     function feedAndMultiply(uint256 _zombieId, uint256 _targetDna) public {
         /* Add a require statement to verify that msg.sender 
         is equal to this zombie's owner */
@@ -33,5 +37,12 @@ contract ZombieFeeding is ZombieFactory {
         uint256 newDna = (myZombie.dna + _targetDna) / 2;
         //父合约方法须为internal而不是private修饰,才能被子合约调用到
         _createZombie("NoName", newDna);
+    }
+
+    function feedOnKitty(uint256 _zombieId, uint256 _kittyId) public {
+        uint256 kittyDna;
+        //处理多返回值 -- 只取最后一个返回值kittyDna
+        (, , , , , , , , , kittyDna) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna);
     }
 }
