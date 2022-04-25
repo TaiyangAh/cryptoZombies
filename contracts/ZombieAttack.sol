d9abb399e69815ca263bfaa7f494c7793e0342e8
TaiyangAh <wulian404@gmail.com>
2022-04-25 18:26:07 +0800

    Random number generation via keccak256

--- contracts/ZombieAttack.sol
+++ contracts/ZombieAttack.sol
@@ -3,4 +3,16 @@ pragma solidity >=0.8.13 <0.9.0;
 
 import "./ZombieHelper.sol";
 
-contract ZombieAttack is ZombieHelper {}
+contract ZombieAttack is ZombieHelper {
+    uint256 randNonce = 0;
+
+    function randMod(uint256 _modulus) internal returns (uint256) {
+        randNonce++;
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(block.timestamp, msg.sender, randNonce)
+                )
+            ) % _modulus;
+    }
+}
